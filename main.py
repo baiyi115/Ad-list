71df26b7e30ce824db827ee7d8e257cec37b43dd
baiyi <1781979985@qq.com>
2025-11-21 22:04:41 +0800

    add mosdns rules and update main.py

--- main.py
+++ main.py
@@ -1,42 +1,76 @@
 import requests
 from datetime import datetime
+import re
+def get_adguard_mixed_list():
+    list_urls = [
+        "https://ghfast.top/raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
+        "https://ruleset.skk.moe/Internal/reject-adguardhome.txt",
+        "https://adrules.top/dns.txt"
+    ]
 
-list_urls = [
-    "https://ghfast.top/raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt",
-    "https://ruleset.skk.moe/Internal/reject-adguardhome.txt",
-    "https://adrules.top/dns.txt"
-]
+    all_rules = set()
 
-all_rules = set()
-
-for url in list_urls:
-    try:
-        response = requests.get(url)
-        response.raise_for_status() 
-
-        lines = response.text.splitlines() 
-
-        for line in lines:
-            line = line.strip() 
-            if line and not line.startswith('#') and not line.startswith('!'):
-                all_rules.add(line)
-        
-        print(f"成功处理: {url}")
-
-    except requests.RequestException as e:
-        print(f"处理失败: {url} - {e}")
-
-sorted_rules = sorted(all_rules, key=lambda x: (not x.startswith('||'), x))
-
-with open("adguard_mixed_list.txt", "w") as f:
-    f.write("! tittle: AdGuard mixed list\n")
-    f.write(f"! build time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
-    f.write(f"! total: {len(all_rules)}\n")
-    f.write("! source:\n")
     for url in list_urls:
-        f.write(f"! {url}\n")
-    f.write("\n")
-    for rule in sorted_rules:
-        f.write(rule + "\n")
+        try:
+            response = requests.get(url)
+            response.raise_for_status() 
 
-print(f"\n 共 {len(all_rules)} 条规则已保存至 adguard_mixed_list.txt")
+            lines = response.text.splitlines() 
+
+            for line in lines:
+                line = line.strip() 
+                if line and not line.startswith('#') and not line.startswith('!'):
+                    all_rules.add(line)
+            
+            print(f"成功处理: {url}")
+
+        except requests.RequestException as e:
+            print(f"处理失败: {url} - {e}")
+
+    sorted_rules = sorted(all_rules, key=lambda x: (not x.startswith('||'), x))
+
+    with open("adguard_mixed_list.txt", "w") as f:
+        f.write("! tittle: AdGuard mixed list\n")
+        f.write(f"! build time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        f.write(f"! total: {len(all_rules)}\n")
+        f.write(f"! homepage: https://github.com/baiyi115/Ad-list")
+        f.write(f"! License: https://github.com/baiyi115/Ad-list/blob/main/LICENSE")
+        f.write("\n")
+        for rule in sorted_rules:
+            f.write(rule + "\n")
+
+    print(f"\n 共 {len(all_rules)} 条规则已保存至 adguard_mixed_list.txt")
+
+def extract_domains(file_path):
+    domains = []
+    keywords=[]
+    with open(file_path, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line or re.search(r'^[!@#]', line):
+                continue
+            match = re.search(r"\|\|([a-zA-Z0-9.-]+)\^", line)
+            if match:
+                if '*' in line:
+                    keywords.append(line)
+                else:
+                    domains.append(match.group(1))
+    return sorted(domains),sorted(keywords)
+
+def mosdns_rules(domains,keywords):
+    with open("mosdns_mixed_list.txt", "w") as f:
+        f.write("# tittle: mosdns_mixed_list\n")
+        f.write(f"# build time: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
+        f.write(f"# total: {len(domains+keywords)}\n")
+        f.write(f"# homepage: https://github.com/baiyi115/Ad-list\n")
+        f.write(f"# License: https://github.com/baiyi115/Ad-list/blob/main/LICENSE\n")
+        for domain in domains:
+            f.write("domain:" + domain + "\n")
+        for keyword in keywords:
+            f.write("keyword:"+ keyword + "\n")
+        print(f"\n 共 {len(domains+keywords)} 条规则已保存至 mosdns_mixed_list")
+
+if __name__ == "__main__":
+    get_adguard_mixed_list()
+    domains,keywords=extract_domains("adguard_mixed_list.txt")
+    mosdns_rules(domains,keywords)
